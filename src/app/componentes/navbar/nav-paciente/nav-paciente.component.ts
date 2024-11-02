29fa36b6cbeea6daad41538791d01dd97cff927d
Agustin Teira <agustin.g.teira@gmail.com>
2024-11-02 10:26:52 -0300

    .

--- src/app/componentes/navbar/nav-paciente/nav-paciente.component.ts
+++ src/app/componentes/navbar/nav-paciente/nav-paciente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-paciente',
@@ -9,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class NavPacienteComponent {
 
+  constructor(private router: Router) {}
+  redirigir(ruta:string) {
+    this.router.navigate([ruta]);
+  }
 }
